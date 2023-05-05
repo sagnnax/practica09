524b090bb553a2b4affda236af3fcad681cab479
sagnnax <axel.gustavo2001@outlook.com>
2023-05-05 10:20:47 -0500

    Mi primer commit

---
+++ src/main/java/org/uv/Practica09/Practica09Application.java
@@ -0,0 +1,13 @@
+package org.uv.Practica09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica09Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/uv/Practica09/Practica09ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uv.Practica09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
